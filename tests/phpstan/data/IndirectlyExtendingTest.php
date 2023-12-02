3d047363d1742668c6f06cbf144288be7d5e6a9a
Marcin Michalski <evulmastah@gmail.com>
2023-12-02 10:39:13 +0000

    Add PHPStan rule enforcing strict mocking (#11)
    Co-authored-by: Ben Challis <ben-challis@users.noreply.github.com>

---
+++ tests/phpstan/data/IndirectlyExtendingTest.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Phpstan\Lendable\PHPUnitExtensions\data;
+
+class IndirectlyExtendingTest extends TestCaseTest {}
---
+++ tests/phpstan/data/StrictMockingTestCaseTest.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Phpstan\Lendable\PHPUnitExtensions\data;
+
+use Lendable\PHPUnitExtensions\TestCase;
+
+final class StrictMockingTestCaseTest extends TestCase {}
